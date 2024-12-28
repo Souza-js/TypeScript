b3c0f6c9a62a2c9aa75efbd60208f2f191b3e6d4
Souza <souzarodrigo0610@gmail.com>
2024-12-28 18:12:00 -0300

    up

--- src/commands/ping.ts
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder, Client } from "discord.js";
-
-export const data = new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Responde com Pong!");
-
-export async function execute(bot: Client, interaction) {
-    await interaction.reply("Pong!");
-}
