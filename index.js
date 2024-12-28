0abb2765211cf3c853cda04936aadeac7805e49f
Guilherme D. F. <guilherme@mischost.com>
2024-12-28 18:02:44 -0300

    Bot de vdd

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
-import { REST, Routes } from "discord.js";
+import { Client } from "discord.js";
+const bot = new Client({ intents: process.env.INTENTS || 0 });
 
 const commands = [];
 const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".ts"));
 
-for (const file of commandFiles) {
-    const command = require(path.join(commandsPath, file));
-    if (command.data) {
-        commands.push(command.data.toJSON());
+fs.readdirSync(commandsPath).filter((file) => file.endsWith(".ts")).forEach(comando=>{
+    let command = require(path.join(commandsPath, comando)
+    if("run" in command && "data" in command){
+        commands.push(command)
     }
-}
+})
+bot.login(process.env.TOKEN)
 
-const rest = new REST({ version: "10" }).setToken("SEU_TOKEN_AQUI");
-
-(async () => {
-    try {
-        console.log("Iniciando o registro de comandos de barra (Slash Commands)...");
-
-        await rest.put(
-            Routes.applicationCommands("SEU_CLIENT_ID"),
-            { body: commands }
-        );
-
-        console.log("Comandos registrados com sucesso!");
-    } catch (error) {
-        console.error(error);
+bot.on("ready", async()=>{
+    bot.application.commands.set(commands.map(t=>t.data))
+    console.log("Logou em", bot.user.tag)
+})
+bot.on("interactionCreate", async i =>{
+    if(i.isCommand()){
+        let c = commands.find(t=>t.data.name === i.commandName)
+        if(c) c.run(bot, i)
     }
-})();
+})
